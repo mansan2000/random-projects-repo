095266d621cca8129f93eb47c851214397aab92e
Mansan2000 <emanuelsanderscode@gmail.com>
2021-02-19 11:57:08 -0600

    adding selenium files to this repo to acces from other computer

---
+++ seleniumTest.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+PATH = "/home/emanuel/Documents/code_test/chromedriver"
+
+driver = webdriver.Chrome(PATH)
+
+driver.get("https://techwithtim.net")
+print(driver.title)
+
+search = driver.find_element_by_name("s")
+search.send_keys("test")
+search.send_keys(Keys.RETURN)
+
+time.sleep(5)
+
+driver.quit()
